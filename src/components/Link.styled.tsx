2dae24591c3bdea19afa1e824cdf6ca5390b5949
Vladislav <labkovich.vl@gmail.com>
2023-11-30 20:59:19 +0300

    feat: add style component

---
+++ src/components/Link.styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+  color: rgba(31,134,189,0.79);
+  font-size: 2rem;
+  font-weight: bold;
+  background-color: transparent; // Задает прозрачный цвет фона для элемента или его заднего плана
+  padding: 0;
+`
